a456161e36b870b5a5289e5fdb822eeb69f6c7d3
morgansliman <morgansliman@gmail.com>
2016-10-24 20:42:14 -0400

    Logic finished. Working on gameplay design next.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
-	function Character (name, health, attack) {
+	function Character (name, health, attack, counter) {
 		this.name = name;
 		this.health = health;
 		this.attack = attack;
+		this.counter = counter;
 		this.img_source = "assets/images/" + name + ".jpg";
 	}
 
@@ -11,12 +12,13 @@ $(document).ready(function() {
 		return this.attack * multiplier;
 	}
 
-	Character.prototype.createElement = function(parent) {
+	Character.prototype.createElement = function(parent, hidden=false) {
 		var div = $("<div>", {
 			"class": "character object-wrapper",
 			"data-name": this.name,
 			"data-health": this.health,
 			"data-attack": this.attack,
+			"data-counter": this.counter
 		});
 
 		var p_name = $("<p>", {
@@ -32,26 +34,34 @@ $(document).ready(function() {
 			"class": "health object-text",
 		}).text(this.health);
 
+		if (hidden) {
+			div.addClass('hid');
+		}
+
 		div.append(p_name, img, p_health);
 		$(parent).append(div);
 	}
 
-	var obiWanKenobi = new Character("Obi-Wan Kenobi", 120, 8);
-	var lukeSkywalker = new Character("Luke Skywalker", 100, 5);
-	var darthSidious = new Character("Darth Sidious", 150, 20);
-	var darthMaul = new Character("Darth Maul", 180, 25);
+	var obiWanKenobi = new Character("Obi-Wan Kenobi", 120, 8, );
+	var lukeSkywalker = new Character("Luke Skywalker", 100, , 5);
+	var darthSidious = new Character("Darth Sidious", 150, , 20);
+	var darthMaul = new Character("Darth Maul", 180, , 25);
+	var ewok = new Character("Ewok", 9001, 9001);
 
 	obiWanKenobi.createElement('.all');
 	lukeSkywalker.createElement('.all');
 	darthSidious.createElement('.all');
 	darthMaul.createElement('.all');
+	ewok.createElement('.all', true);
 
 	var balance = {
 		'Obi-Wan Kenobi': obiWanKenobi,
 		'Luke Skywalker': lukeSkywalker,
 		'Darth Sidious': darthSidious,
-		'Darth Maul': darthMaul
+		'Darth Maul': darthMaul,
+		'Ewok': ewok
 	}
+	
 
 	$(".object-wrapper").on("click", function() {
 		if ($('.player').children().length == 0) {
@@ -80,20 +90,79 @@ $(document).ready(function() {
 
 	var i = 1;
 	$('.attack-button').on('click', function() {
+		if ($('.defender').children().length == 0) {
+			$('.top').text('No enemy here');
+			$('.top').css('display', 'block');
+		}
+
 		var player = $('.player').children()[0];
 		var defender = $('.defender').children()[0];
 
-		var playerChar = balance[$(player).data('name')];
-		var defenderChar = balance[$(defender).data('name')];
+		if ($(player).data('health') >= 0 && $(defender).data('health') >= 0) {
 
-		$('.status-text').css('display', 'block');
-		$('.top').text(
-			'You attacked ' + $(defender).data('name') + ' for ' + playerChar.damage(i) + ' damage.' 
-		);
-		$('.bottom').text(
-			$(defender).data('name') + ' attacked you back for ' + $(defender).data('attack') + ' damage.'
-		);
+			var playerChar = balance[$(player).data('name')];
+			var defenderChar = balance[$(defender).data('name')];
 
-		i += 1;
+			var playerHealth = $(player).find('.health');
+			var defenderHealth = $(defender).find('.health');
+
+			$(playerHealth).text(
+				parseInt($(player).data('health')) - parseInt($(defender).data('counter'))
+			);
+			$(player).data('health', $(playerHealth).text());
+
+			$(defenderHealth).text(
+				parseInt($(defender).data('health')) - playerChar.damage(i)
+			);
+			$(defender).data('health', $(defenderHealth).text());
+
+			$('.status-text').css('display', 'block');
+			$('.top').text(
+				'You attacked ' + $(defender).data('name') + ' for ' + playerChar.damage(i) + ' damage.' 
+			);
+			$('.bottom').text(
+				$(defender).data('name') + ' attacked you back for ' + $(defender).data('counter') + ' damage.'
+			);
+
+			i += 1;
+		}
+
+		if ($(player).data('health') <= 0) {
+			$('.top').text('You have been defeated...GAME OVER!!!');
+			$('.bottom').text('');
+			$('.bottom').css('display', 'none');
+			$('.restart-button').css('display', 'block');
+		}
+
+		else if ($(defender).data('health') <= 0) {
+
+			if ($('.enemy').children().length == 0) {
+				$(defender).remove();
+				$('.top').text('You Won!!!! GAME OVER!!!');
+				$('.bottom').text('');
+				$('.bottom').css('display', 'none');
+				$('.restart-button').css('display', 'block');
+			}
+
+			else {
+				$('.top').text(
+					'You have defeated ' + 
+					$(defender).data('name') + 
+					', you can choose to fight another enemy.'
+				);
+				$('.bottom').text('');
+				$(defender).remove();
+			}
+		}
+	});
+
+	$('.restart-button').on('click', function() {
+		location.reload();
+	});
+
+	var easterEgg = new Konami(function() {
+		$('.all').children('[data-name="Ewok"]').css('display', 'block');
+		$('.top').text('JubJubJub...');
+		$('.top').css('display', 'block');
 	});
 });
